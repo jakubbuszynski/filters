00f7ddb8d04d1c05852cd29c163a7a06aaaeea7f
cuttarug <lpolepeddi@gmail.com>
2013-04-03 17:03:08 -0600

    Blur

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+var filters = {};
+
+var update_filters = function(filter, value, units) {
+  if(value == 0) {
+    delete filters[filter];
+  }
+  else {
+    filters[filter] = value + units;
+  }
+}
+
+var update_output = function() {
+  var filter_values = [];
+  $.each(filters, function(k, v) {
+    filter_values.push(k + '(' + v + ')');
+  });
+  var style = filter_values.join(' ');
+  $('.output span').text(style);
+  $('#e1 img').css('-webkit-filter', style);
+}
+
+var update = function(filter, value, units) {
+  update_filters(filter, value, units);
+  update_output();
+}
+
+$('input[name="blur"]').change(function() {
+  update('blur', this.value, 'px');
+});
