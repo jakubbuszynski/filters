0e891279452c2c8ae2049ae3381c98ab379dd73f
cuttarug <lpolepeddi@gmail.com>
2013-04-07 23:56:31 -0600

    Lomo

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,25 +78,30 @@ $('#reset').click(function() {
 
 $('#glowing-sun').click(function() {
   $(this).toggleClass("down").siblings().removeClass('down');
-  $('#e2 img').toggleClass('glowing-sun').removeClass('grungy pinhole sin-city vintage');
+  $('#e2 img').toggleClass('glowing-sun').removeClass('grungy lomo pinhole sin-city vintage');
 });
 
 $('#grungy').click(function() {
   $(this).toggleClass("down").siblings().removeClass('down');
-  $('#e2 img').toggleClass('grungy').removeClass('glowing-sun pinhole sin-city vintage');
+  $('#e2 img').toggleClass('grungy').removeClass('glowing-sun lomo pinhole sin-city vintage');
+});
+
+$('#lomo').click(function() {
+  $(this).toggleClass("down").siblings().removeClass('down');
+  $('#e2 img').toggleClass('lomo').removeClass('glowing-sun grungy pinhole sin-city vintage');
 });
 
 $('#pinhole').click(function() {
   $(this).toggleClass("down").siblings().removeClass('down');
-  $('#e2 img').toggleClass('pinhole').removeClass('glowing-sun grungy sin-city vintage');
+  $('#e2 img').toggleClass('pinhole').removeClass('glowing-sun grungy lomo sin-city vintage');
 });
 
 $('#sin-city').click(function() {
   $(this).toggleClass("down").siblings().removeClass('down');
-  $('#e2 img').toggleClass('sin-city').removeClass('glowing-sun grungy pinhole vintage');
+  $('#e2 img').toggleClass('sin-city').removeClass('glowing-sun grungy lomo pinhole vintage');
 });
 
 $('#vintage').click(function() {
   $(this).toggleClass("down").siblings().removeClass('down');
-  $('#e2 img').toggleClass('vintage').removeClass('glowing-sun grungy pinhole sin-city');
+  $('#e2 img').toggleClass('vintage').removeClass('glowing-sun grungy lomo pinhole sin-city');
 });
